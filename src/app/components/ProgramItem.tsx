042c09a4d6d024e2a19445c1ad6fb00ae46ca42b
Akhil VC <akhilvc10@gmail.com>
2024-08-10 11:55:14 +0400

    style

--- src/app/components/ProgramItem.tsx
+++ src/app/components/ProgramItem.tsx
@@ -26,7 +26,7 @@ export const Program = ({ program, ...rest }: ProgramItem) => {
 	return (
 		<ProgramBox width={styles.width} style={styles.position}>
 			<ProgramContent width={styles.width} isLive={isLive}>
-				<ProgramFlex>
+				<div className="flex w-full [&_p]:text-lg [&_p]:font-bold [&_p]:text-gray-900  [&_p]:hover:text-white [&_span]:hover:text-white [&_span]:hover:text-white-50">
 					{<ProgramImage src={image} alt="Preview" />}
 					<ProgramStack>
 						<ProgramTitle>{title}</ProgramTitle>
@@ -34,7 +34,7 @@ export const Program = ({ program, ...rest }: ProgramItem) => {
 							{sinceTime} - {tillTime}
 						</ProgramText>
 					</ProgramStack>
-				</ProgramFlex>
+				</div>
 			</ProgramContent>
 		</ProgramBox>
 	);
